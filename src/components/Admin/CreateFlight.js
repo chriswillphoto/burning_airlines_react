08e7134487c9a00a75d82f9b3b0b0a9ba179601d
Sam Cordingley <sam@cordandberg.com>
2017-12-12 18:17:18 +1100

    created components no structure

---
+++ src/components/Admin/CreateFlight.js
@@ -0,0 +1,14 @@
+import React, { PureComponent as Component } from 'react';
+
+class CreateFlight extends Component {
+  render() {
+    return(
+      <div>
+        <form>
+          <input
+          <input type="submit" value="Add Flight" />
+        </form>
+      </div>
+    );
+  }
+}
